4493783f5a255ee6e35c3b04897c051b8b921ea7
Manfred Steyer <manfred.steyer@gmx.net>
2022-11-14 23:50:59 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [
+    NavbarComponent,
+    SidebarComponent,
+  ]
 })
 export class AppComponent {
-  title = 'essentials';
+  title = 'Hello World!';
 }
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterLinkWithHref } from '@angular/router';
+
+@Component({
+    standalone: true,
+    selector: 'app-sidebar-cmp',
+    templateUrl: 'sidebar.component.html',
+    imports: [
+    ]
+})
+export class SidebarComponent {
+}
